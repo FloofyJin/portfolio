6aa5d26a2d63e08cc0782ccfe9684a296f50ad54
floofyjin <jinsungpark333@gmail.com>
2022-06-30 00:18:12 -0700

    helmet title

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
 
+import { Helmet } from 'react-helmet';
+
 import Home from './Home';
 import Huffman from './Huffman';
 
@@ -10,6 +12,10 @@ function App() {
 
   return (
     <Router>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Jinsung Park - Portfolio</title>
+      </Helmet>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/huffman' element={<Huffman />} />
