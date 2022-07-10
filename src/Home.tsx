d9f6cffc685f94a1486a61be04325c4adbd39526
floofyjin <jinsungpark333@gmail.com>
2022-07-10 11:32:09 -0700

    grr

--- src/Home.tsx
+++ src/Home.tsx
@@ -21,7 +21,7 @@ function Home() {
         <div className="overall">
             <div className="header">
                 <div className="resume">
-                    <a href="https://drive.google.com/file/d/1n1sI1kyR1VpMjgAJzjhqHFzs0lnveMZX/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1gRBoJ5mqNkH-SJscv-A-J5Fv36UHE0Cr/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                         <img className="logo-icon" alt="Qries" src={resume_icon} />
                     </a>
                 </div>
